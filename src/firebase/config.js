1311d4cb6234418c86ea4027df6aca30988787ca
Felipe Costa <felipecosta776@outlook.com>
2023-10-04 22:17:25 -0300

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLUaL7sE_TqBnNy7yQdOHd7_XX7eUMxe0",
+  authDomain: "picoposts.firebaseapp.com",
+  projectId: "picoposts",
+  storageBucket: "picoposts.appspot.com",
+  messagingSenderId: "989595295289",
+  appId: "1:989595295289:web:ccb601f402bcbbddf0f7e8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
