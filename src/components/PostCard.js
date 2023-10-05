8ef16db8c1b9545081fa952a73538fd6d29f1b6e
Felipe Costa <felipecosta776@outlook.com>
2023-10-04 23:14:21 -0300

    error object fetch fix

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,15 +1,26 @@
+import { doc, deleteDoc } from "firebase/firestore";
+import { auth, db } from "../firebase/config";
+
 export const PostCard = ({ post }) => {
-  const { title, description, author } = post;
+  const { id, title, description, author } = post;
+  const isAuth = JSON.parse(localStorage.getItem("isAuth"));
+
+  async function handleDelete() {
+    const document = doc(db, "posts", id);
+    await deleteDoc(document);
+  }
 
   return (
     <div className="card">
       <p className="title">{title}</p>
       <p className="description">{description}</p>
       <p className="control">
-        <span className="author">{author}</span>
-        <span className="delete">
-          <i className="bi bi-trash3"></i>
-        </span>
+        <span className="author">{author.name}</span>
+        {isAuth && author.id === auth.currentUser.uid && (
+          <span onClick={handleDelete} className="delete">
+            <i className="bi bi-trash3"></i>
+          </span>
+        )}
       </p>
     </div>
   );
