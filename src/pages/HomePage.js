2c68abffff768670dc3a7803262216adf73780e3
Felipe Costa <felipecosta776@outlook.com>
2023-10-04 21:57:13 -0300

    homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,28 @@
+import { PostCard } from "../components";
+
 export const HomePage = () => {
-    return (
-      <div>HomePage</div>
-    )
-  }
-  
+  const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet.",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit nulla sequi tempore! Perferendis tempora, quas debitis doloribus cumque odit perspiciatis?",
+      author: "Costa",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet.",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit nulla sequi tempore! Perferendis tempora, quas debitis doloribus cumque odit perspiciatis?",
+      author: "Costa",
+    },
+  ];
+
+  return (
+    <section>
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </section>
+  );
+};
